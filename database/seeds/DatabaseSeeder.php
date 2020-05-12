1caa7604cc77b73a6c1370c224e4ed4567fbf2c2
afnan <afnan_samy@icloud.com.com>
2020-05-13 01:06:33 +0200

    message

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,6 @@ class DatabaseSeeder extends Seeder
     // afnan
     public function run()
     {
-
-         $this->call(LaratrustSeeder::class);
+      $this->call(LaratrustSeeder::class);
     }
 }
